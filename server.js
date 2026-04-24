102fd84920057fa753223634351a08845a325357
Nikhil034 <nikhilsondarava@lampros.tech>
2026-04-24 18:31:16 +0530

    feat:Added PEER FINGERPRINT and ack of package for the delivered,send and received and added ECDSA alogithm.

--- server.js
+++ server.js
@@ -1,43 +1,76 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const path = require('path');
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-// In-memory key store: socketId -> publicKey (JWK)
+// In-memory key store: socketId -> exported public keys (JWK)
 const publicKeys = {};
 
-io.on('connection', (socket) => {
-  console.log('Client connected:', socket.id);
+function getPeerList() {
+  return Object.entries(publicKeys).map(([id, keys]) => ({
+    id,
+    encryptKey: keys.encryptKey,
+    signKey: keys.signKey,
+  }));
+}
+
+function broadcastPeerList() {
+  io.emit("peer-list", getPeerList());
+}
+
+io.on("connection", (socket) => {
+  console.log("Client connected:", socket.id);
 
   // Client registers their public key
-  socket.on('register-key', (jwkPublicKey) => {
-    publicKeys[socket.id] = jwkPublicKey;
-    // Broadcast updated peer list (id + public key) to everyone
-    io.emit('peer-list', Object.entries(publicKeys).map(([id, key]) => ({ id, key })));
-    console.log(`Key registered for ${socket.id}`);
+  socket.on("register-key", ({ encryptKey, signKey }) => {
+    publicKeys[socket.id] = { encryptKey, signKey };
+    broadcastPeerList();
+    console.log(`Keys registered for ${socket.id}`);
   });
 
   // Relay encrypted message to target peer only
-  socket.on('send-message', ({ to, ciphertext, iv, senderPublicKey }) => {
-    io.to(to).emit('receive-message', {
-      from: socket.id,
-      ciphertext,
-      iv,
-      senderPublicKey,
-    });
+  socket.on(
+    "send-message",
+    ({ to, ciphertext, iv, senderPublicKey, senderSigningKey, signature, msgId }) => {
+      if (!to) {
+        return;
+      }
+
+      io.to(to).emit("receive-message", {
+        from: socket.id,
+        ciphertext,
+        iv,
+        senderPublicKey,
+        senderSigningKey,
+        signature,
+        msgId,
+      });
+    },
+  );
+
+  // Relay delivered ack back to original sender
+  socket.on("msg-delivered", ({ to, msgId }) => {
+    io.to(to).emit("msg-delivered", { msgId });
   });
 
-  socket.on('disconnect', () => {
+  // Relay read ack back to original sender
+  socket.on("msg-read", ({ to, msgId }) => {
+    io.to(to).emit("msg-read", { msgId });
+  });
+
+  socket.on("disconnect", () => {
     delete publicKeys[socket.id];
-    io.emit('peer-list', Object.entries(publicKeys).map(([id, key]) => ({ id, key })));
-    console.log('Client disconnected:', socket.id);
+    broadcastPeerList();
+    console.log("Client disconnected:", socket.id);
   });
 });
 
-server.listen(3000, () => console.log('Server running at http://localhost:3000'));
+server.listen(3000, () =>
+  console.log("Server running at http://localhost:3000"),
+);
